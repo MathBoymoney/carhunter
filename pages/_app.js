95ea1247564aae6108188f87d171a5603bcb25fe
MathBoymoney <tanquerel.mathis@gmail.com>
2025-12-02 19:48:03 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-// pages/_app.js
-import '../styles/globals.css';
+import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
